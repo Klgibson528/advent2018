ed45b2c0570215676e7ae6f7011edd39dd397cf4
Katy Gibson <klgibson528@gmail.com>
2018-12-03 15:08:23 -0600

    finished day2 part 1

---
+++ day2.py
@@ -0,0 +1,291 @@
+data = '''ubkfmdjxyzlbgkrotcepvswaqx
+uikfmdkuyzlbgerotcepvswaqh
+uikfmdpxyzlbgnrotcepvswoeh
+nikfmdjxyzlbgnrotqepvswyqh
+qgkfmdjxyzlbgnmotcepvswaqh
+uikfmdjxyzqbgnrytcepvsbaqh
+uikfmdjxyzibgprotcecvswaqh
+uikfmajxyzlcgnrojcepvswaqh
+uvkfsdjxyzlbgnrotcepvjwaqh
+uikfmdjxfzlbggrotcepvswawh
+uikfmhjxyzlbgnuotcepvjwaqh
+uikfmdjxyzlbuzcotcepvswaqh
+uikfmdwxyzlbgnrotcepvfwamh
+uikfmdexyzlbgnroecepvswhqh
+uikfmdjuyzlbgnrotcqpvswafh
+uikfddjxyzvbgnrotceppswaqh
+yikfwdjxyzlbgnrotcepvswagh
+uiktmdjxyzlbgnrotceposwajh
+uikfmdsxyzlbgnroteetvswaqh
+uikfpdjxyzlbgnroncepvswuqh
+uikfmtjxyzlbgurotcepvswaoh
+eikfmdjxyjlbgnrotcepyswaqh
+uikfkdjxyzlbgnrotcepvszaqv
+uikfrdjxtwlbgnrotcepvswaqh
+uikfmdjxyzlbgnrotpepwswahh
+kikfmdjxyzlbgnkotcepvswqqh
+uikfkduxyzlbgnrotcepvswafh
+uikfxhjxyzlbgnrotcegvswaqh
+uikfmdjxyzlmgnrotcenvawaqh
+uzkfmddxyzlbgnrltcepvswaqh
+uikfmdjxyzlbgnrobcepisqaqh
+uijfmdjxyzlbgnrotcexvrwaqh
+uiwjmdjxyzlbgnrotceprswaqh
+uhkqmdjxwzlbgnrotcepvswaqh
+uiktmsjxyzwbgnrotcepvswaqh
+uikfmdjxyztbgnqotcepvswcqh
+uibfmdjxyzlbgnroqcepvswaqx
+uwkfmdjxyxlbgnrotcfpvswaqh
+uikumdjxyzlbgnrstceposwaqh
+uikfzdjxyznhgnrotcepvswaqh
+uikuydjxyzlbgnrotqepvswaqh
+uikfmdqxyzlbgnrotfefvswaqh
+yikfmdjxyzlbrnrqtcepvswaqh
+uiifmdjxyzlbenrotcfpvswaqh
+uxkjmdjxyzlbgnrotcmpvswaqh
+uikgmdjxyzlbgnrotceovlwaqh
+uikfmdjxyzvbgzrotcenvswaqh
+uiufmdjxyzlbgnrotceposwazh
+uiafmdjxyzlignmotcepvswaqh
+uikfmdjxyzwbgnsotlepvswaqh
+uikjmdjvyzlbgnrotcenvswaqh
+uikfmdjxyzlbonroteepvswaqi
+uikfmdjxyzldgnroxcepviwaqh
+uikvmdjxyzlbgdrotrepvswaqh
+uikfmdjyyzwbgnrotcepvzwaqh
+uikfmdjxyzzbnnvotcepvswaqh
+uikomdjxyzlbgnrotcepvuwhqh
+uikfmmjxyzbbgnrotcepvswayh
+uikfmdjfezlbgprotcepvswaqh
+uzkfmojxmzlbgnrotcepvswaqh
+uipfmdjxyzlbgnrotceyuswaqh
+uikfmdjxyzlkgnmotcepvswadh
+uikfmdjxyzlbgnuctcbpvswaqh
+uikfqdjxyzlbogrotcepvswaqh
+uikfmdjxyzlfynrotcepvswash
+uikfmdjxizzmgnrotcepvswaqh
+uhkfmdjxyzlbhnrotcenvswaqh
+uipfmdjxyzlbgorotcepfswaqh
+uikfmdjxyznbgnrotcepvswfah
+uikfmujxyzlbgnrotnepvssaqh
+uikfmdjxyzlxgnrotcepvsrwqh
+uikfmdjxszlbgnrottvpvswaqh
+umkfmdskyzlbgnrotcepvswaqh
+uikfmdjxyzlbgorotcwpzswaqh
+uikfmdhxyzzbgnzotcepvswaqh
+jikfmdjxyzlbgnrotcepveyaqh
+uirfmdlxyzlbgnjotcepvswaqh
+uikfmdjxyzlbgnrorxepvswazh
+uikfmdjxyzltgnrotcepvsxaqi
+uikfmdjxyzlbxlrotcepkswaqh
+uvkfmdjxyzlbgnrotcopvswxqh
+uxkfmdjxkzlbgnrotcepvswqqh
+uikfmdjxyzlbqnrotcepvhwrqh
+uikfmdjxyzlvgnrolcepvswrqh
+urkfmdixyzlbgnrotcepvsjaqh
+uikfmdjxymlbsnrotcepvswcqh
+uikfmdjxyilbgnrotcepvcwzqh
+uikfadjxlzlbgnrotcepvswaql
+uikfmdjxuzlbgdrotcgpvswaqh
+yikfmdgxyzlbgnrotcepvswarh
+uikfmdjxyzlbgorotcepcswaqv
+uikkmdjxyzlbvnrotcepvvwaqh
+uwzfmdjxyxlbgnrotcfpvswaqh
+uikfmdjxyztbgnrotcrtvswaqh
+uiufmdjxyzhbgnrotcupvswaqh
+uikfzdjcyzlbgnrotcfpvswaqh
+uipfmdjxyzlbgnrotavpvswaqh
+uikfmajxyzlbgnrotcepbsxaqh
+uikfmdjfyzlbgnrotbepvswmqh
+gikkmdjxyzlbgnrptcepvswaqh
+uikfmdjxqvlbgnrotsepvswaqh
+fikfmdjxyzlbgnrotcegvswoqh
+idkfmdjxyzlbgnrotcepwswaqh
+uikfmdqxyzlbmnrobcepvswaqh
+uikfmdjxyzpbgnroicepvsyaqh
+uikfmkoxyzlbgnrotcgpvswaqh
+unkfmdjxyzlbpnrolcepvswaqh
+uikfmdjmyzlbgfrotcupvswaqh
+ujkfmdjxynlbgnroteepvswaqh
+uikfmljxyzlbgnaotcepvsiaqh
+uikfmdjdyzlbgnrotcepvtwaqd
+uikfmdjxyzlbgnyotcepimwaqh
+uikfmdjxyzfbjnrotcepvxwaqh
+eiwfmdjxyzlbgnrdtcepvswaqh
+umkhmdjxyzlbgnrotceivswaqh
+uikfmdjxyzlbgnrotcwpvswneh
+uckfmdjxyzlbknrotcepvswauh
+uiofmdjoyzlbgnrbtcepvswaqh
+uikfmdbxyzlbgnrotcepaslaqh
+uimfmdjxyalbgnrotcepvswaxh
+uqkfmdjxyzlbwnrotcepmswaqh
+uiyfmdjxyzlbgnrotcepvswxuh
+uikfmdjxyzlbgmrotgepvswamh
+uikfmdjxyqlbgarozcepvswaqh
+uikfmdjxyzabanpotcepvswaqh
+uikfmdjgyzlbsnrotcepvswaqj
+uikfmdjxyzlbwnrottepvsvaqh
+uikfmdjxyzlbbnrotcepvofaqh
+uikfudjxyzlbgnustcepvswaqh
+cskfmqjxyzlbgnrotcepvswaqh
+uiwfmddxyzlbgnrotccpvswaqh
+eikpmdjxyzlbgnrotcesvswaqh
+uikfmdzxyzlngnrrtcepvswaqh
+uikfmdjxyzlbgnrotcepaswtph
+uirfmdjxyzlbgnrotcepvswsqe
+uikjmdjxqzlbgirotcepvswaqh
+uikfmdjxsllbknrotcepvswaqh
+uikfmdjxyqlbgcrotcepvswaqu
+uikfmdjsymlbgnrotcebvswaqh
+uikfmdjxezlbgnroccepviwaqh
+uikfmdjiyzjbgnrotcepvswarh
+uqkfmdjxyzmbgnrotcepvslaqh
+unkfmdjxyzlbinrotceplswaqh
+uikfmdjxyzpbgnrjtcedvswaqh
+uicfmdjxyzlbgrrotcepvswamh
+ukknmdjxyzlbgnqotcepvswaqh
+uikfudjxyzlbdnrztcepvswaqh
+uikfmdjxyzlbgnrozcepvswawk
+uikfmduxyzsbgnrotcepvswauh
+uikfmdjxyzljbnrotcenvswaqh
+uukfmdjxyzlbgnrotcckvswaqh
+uilfldjxyzlbgnrotcdpvswaqh
+uckfmdjxyvybgnrotcepvswaqh
+qikfmdjxyglbgnrotcepvrwaqh
+uikfmhjxyzltgnrotcepvswbqh
+uikfmdjxipabgnrotcepvswaqh
+uikfmdjxyzlbgnrotceovswanl
+uikfmdjxyzlbgirotcapvswahh
+uikfucjxyflbgnrotcepvswaqh
+pikfmdjxyzpbgnrotcepvswaeh
+uikfmdjiyylbgnrotcervswaqh
+uikfmdjgyzlbgnrotcaevswaqh
+uikvmdjxyzlbunrotcepvswarh
+uikfadjuyzpbgnrotcepvswaqh
+uikfmdjxyzlbgnrotcepsawaqj
+eikfmdjxyflbgnrotcepvswaeh
+uisfmdaxyzlbgnrotcepvswlqh
+vikfmdjxyzlzgnrotcepvswanh
+ukkfmdoxyzlbgnrotcewvswaqh
+uikfmdhxyzlbgnrotcrpvbwaqh
+uhkfmdjwezlbgnrotcepvswaqh
+uikfddjxyzlbgnroteepvpwaqh
+uikfmdjxczlbgncotiepvswaqh
+uikfvdjxyzlbgnrotcnpvsaaqh
+uikfmdjxyzlbgnritcepvlwmqh
+uikfmdjxczlcgnrotcecvswaqh
+mikfmdjxyzlbgnrotcepvswasi
+uifvmdjxyzlbgnrotpepvswaqh
+uikzmdjxyzlbgnrotrepvswaqs
+uikfmqjqyzlbunrotcepvswaqh
+uikfpdyxyzlbgnrotcepvswagh
+uikfmdjxyzobgnrotrlpvswaqh
+zisdmdjxyzlbgnrotcepvswaqh
+uikfmdjxyzlbgnlotiesvswaqh
+uikfddixyzlbgnroucepvswaqh
+uijfmdrxyzlbgnrotoepvswaqh
+uikfmdcxbzlbgnrotcepvpwaqh
+uikfmdjxxzlbfnrotcecvswaqh
+upkfmdjxyzmtgnrotcepvswaqh
+uikfmdrxyzlbgnrjtcepvswaqp
+uizfmdjxyzlbsnrotcepviwaqh
+uidfmdjxyslbgnrotcxpvswaqh
+uikfmdjxyzlbgnrovyepvsdaqh
+uiafmdjxyzlbgnrxtcepvsdaqh
+ugkfmdjxyzlbgnrodcepvswawh
+pikfmtjxyzhbgnrotcepvswaqh
+uikfmdjxyzlfgnvotcepvswtqh
+uikbmdjxyzlbgerotcepvswaqm
+uikfmdjxyhlbdnrotcepvswaqy
+uikfgdjxyzlbgnhotcepvswdqh
+uikfmdpxyzlbgnrotcepvscanh
+uikfmdjxyzsbgnretcepvswaqn
+uikfddjxyzlrgnrotcepvsbaqh
+uikfmdjxyzlbgnrotcqnrswaqh
+uhkfmejxyzlbgnrotvepvswaqh
+uikimdjxyzlngnrotceprswaqh
+uikfmdjxyzwbunrotiepvswaqh
+rikfmdjxyzlbgnrotcypvssaqh
+uikfmdjxyzlbdnrotcrpvswsqh
+uekfmdjxkzlbznrotcepvswaqh
+uikfmdjxyglbgvrotcepvswaqv
+uikfmcjxyzlbgnrotmeovswaqh
+uikfmdjxyznbgnrozcepvswaqm
+uikfmdjxyzlbdnrotcepdsyaqh
+umkfmdjxfzlbgnrotiepvswaqh
+uitfmdjxyzvbcnrotcepvswaqh
+uikfmdjqyzlbgnrvtcepvlwaqh
+uikfmdjxyzkbworotcepvswaqh
+uikfmdzxyzlbwnrotcypvswaqh
+uikfmdjxyklbgnrftyepvswaqh
+uinfmsjxyzlbgnrotcepsswaqh
+xisfmdjxymlbgnrotcepvswaqh
+uikfmdjxjzlbgnropcepvswaqv
+uikfmdjxyalegyrotcepvswaqh
+uikfydjxyzlbgnrotcekvswtqh
+uikwmojxyzlbgnromcepvswaqh
+uikdmdjayzlbgnrotcepvswzqh
+uikfmdjxyzlmvnrotctpvswaqh
+uikfmbjxyzlbgnrotceptsweqh
+yvkfmdjxyzlbgqrotcepvswaqh
+uikomdjxfxlbgnrotcepvswaqh
+uikfmdjxczlbgnroocepgswaqh
+uikemdjxizlbgnrotcegvswaqh
+uikdmdjxyzlbgwrotceprswaqh
+tiyfmdjfyzlbgnrotcepvswaqh
+tmkfmdjxyzlbgirotcepvswaqh
+uikfmdjxyzebgnzotcepqswaqh
+uikfmljxyzlbgnrwtcepvswaeh
+uikfmojxyzlbgnrotcepbswwqh
+uikfmdjxyzlbgsrotcewvswwqh
+uikfmdjhyzlbgdrogcepvswaqh
+uikfmvjxyzlbrnrltcepvswaqh
+jikfmdjxyzlbgnrotcepvgcaqh
+uikhtdjxyzlbgnrotcepvswarh
+uikfmdjxyezbgnritcepvswaqh
+uikfwdjxyzlbgnrotzepvsnaqh
+uikfmdjxqylbgnrobcepvswaqh
+zikzmdjxyzlbgnrhtcepvswaqh
+uiksmzjxyzlbgnrftcepvswaqh
+uikfmdjxuzlbgnrotcepvsvaqc
+'''
+
+# Challenge 1
+# listdata = data.splitlines()
+# list_of_dicts = []
+# totalDoub = 0
+# totalTrip = 0
+
+# # creates a list(list_of_dicts) containing a dictionary for each string
+# for i in listdata:
+#     new_dict = {}
+#     for a in i:
+#         if a in new_dict:
+#             new_dict[a] += 1
+#         else:
+#             new_dict[a] = 1
+#     list_of_dicts.append(new_dict)
+
+# # prints how many doubles and triples there are in a dictionary
+# for dic in list_of_dicts:
+#     doub=0
+#     trip=0
+#     for i in dic:
+#         if dic[i] == 2:
+#             doub+=1
+#         elif dic[i] == 3:
+#             trip+=1
+#         else:
+#             pass
+#     if doub > 0:
+#         totalDoub +=1
+#     if trip > 0:
+#         totalTrip +=1
+
+# print(totalDoub * totalTrip)
+
+
+# Challenge 2
+
+
+
